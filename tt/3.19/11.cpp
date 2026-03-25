da47ce8ddc6231074f8498fc7d46b50942583a83
Sophia7320 <2060205344@qq.com>
2026-03-26 02:40:08 +0800

    11 complete

--- tt/3.19/11.cpp
+++ tt/3.19/11.cpp
@@ -1,7 +1,8 @@
+#include <stdint.h>
 #include <iostream>
+#include <queue>
 #include <string>
 #include <vector>
-#include <queue>
 
 using namespace std;
 using ll = long long;
@@ -11,16 +12,30 @@ const int N = 1e6 + 10;
 int n, m, k, p;
 int s, t;
 
+struct node {
+    int id, dis, v;
+
+    auto operator<(const node& other) const {
+        if (dis == other.dis)
+            return v < other.v;
+        else
+            return dis > other.dis;
+    }
+};
+
 vector<ll> road[N];
 ll val[N];
-priority_queue<pair<ll, ll>> pq;
+priority_queue<node> pq;
 bool visited[N];
-ll ans[N];
-
+ll ans_v[N];
+ll ans_dis[N];
 
 void solve() {
     cin >> n >> m >> k >> p;
-    for (int i = 1;i <= n;i++) cin >> val[i];
+    for (int i = 0; i <= n; i++)
+        ans_dis[i] = INT64_MAX;
+    for (int i = 1; i <= n; i++)
+        cin >> val[i];
 
     while (m--) {
         int u, v;
@@ -31,26 +46,55 @@ void solve() {
 
     cin >> s >> t;
     int c = 1;
-    pq.push({ val[s],s });
+
+    node st;
+
+    st.v = p == 1 ? val[s] : 0;
+    st.id = s, st.dis = 0;
+
+    pq.push(st);
+    ans_v[s] = st.v;
+    ans_dis[s] = 0;
+
     while (!pq.empty()) {
-        ll u = pq.top().second;
-        ll v_sum_cur = pq.top().first;
+        ll u = pq.top().id;
+        ll v_sum_cur = pq.top().v;
+        ll dis_sum_cur = pq.top().dis;
 
         pq.pop();
-        if (v_sum_cur < ans[u]) continue;
+        if (dis_sum_cur != ans_dis[u] || v_sum_cur != ans_v[u])
+            continue;
+
+        // if (visited[u])
+        //     continue;
 
         visited[u] = true;
 
-        for (auto& nei : road[u]) {
+        for (auto nei : road[u]) {
+            node nn;
+            nn.id = nei;
+            nn.dis = dis_sum_cur + 1;
+            if ((nn.dis) % k + 1 == p) {
+                nn.v = v_sum_cur + val[nei];
+            } else {
+                nn.v = v_sum_cur;
+            }
 
+            if (!visited[nei]) {
+                if (nn.dis < ans_dis[nei]) {
+                    pq.push(nn);
+                    ans_dis[nei] = nn.dis;
+                    ans_v[nei] = nn.v;
+                } else if (nn.dis == ans_dis[nei] && nn.v > ans_v[nei]) {
+                    pq.push(nn);
+                    ans_dis[nei] = nn.dis;
+                    ans_v[nei] = nn.v;
+                }
+            }
         }
-
-
-
     }
 
-
-
+    cout << ans_v[t] << endl;
 }
 
 int main() {
@@ -59,7 +103,7 @@ int main() {
     cout.tie(nullptr);
 
     int t = 1;
-    //cin >> t;
+    // cin >> t;
     while (t--) {
         solve();
     }
