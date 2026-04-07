ff4bccfc730f0881922223b5be6586eb09abf6da
Sophia7320 <2060205344@qq.com>
2026-04-08 02:31:47 +0800

    P6345

--- luogu/P6345.cpp
+++ luogu/P6345.cpp
@@ -12,76 +12,55 @@ using ll = long long;
 
 const ll N = 1e6 + 10;
 
-vector<ll> ii;
-vector<ll> v(51);
-vector<vector<ll>> vc(51);
-int n;
-bool ans[1100000];
+struct qwq {
+    priority_queue<int, vector<int>, greater<int>> qmi;
+    priority_queue<int> qma;
+
+    qwq() : qma(), qmi() {}
+
+    int getans() {
+        return qma.top();
+    }
+
+    void insert(int n) {
+        if (qmi.empty() && qma.empty()) {
+            qma.push(n);
+            return;
+        }
+        int t = getans();
+        if (n < t)
+            qma.push(n);
+        else
+            qmi.push(n);
+
+        keep();
+    }
+
+    void keep() {
+        int now = (qmi.size() + qma.size() + 1) / 2;
+        while (qma.size() != now) {
+            if (qma.size() > now) {
+                qmi.push(qma.top());
+                qma.pop();
+            } else {
+                qma.push(qmi.top());
+                qmi.pop();
+            }
+        }
+    }
+};
 
 void solve() {
+    qwq q;
+
+    int n;
     cin >> n;
-    ii.reserve(n);
-    for (int i = 1; i <= 50; i++) {
-        vc.reserve(50);
-        v.reserve(50);
-    }
-    while (n--) {
+    for (int i = 1; i <= n; i++) {
         int t;
         cin >> t;
-        ii.push_back(t);
-        v[t]++;
-    }
-    int m1 = -1, m2 = -1;
-    for (int i = 50; i >= 1; i--) {
-        if (v[i] > 1) {
-            m1 = m2 = i;
-            break;
-        } else if (v[i] == 1) {
-            m1 = i;
-            i--;
-            while (i >= 0 && v[i] == 0)
-                i--;
-            m2 = i;
-            break;
-        }
-    }
-    for (int i = 50; i >= 1; i--) {
-        if (!v[i])
-            continue;
-        int p = i + 1;
-        while (p <= 50) {
-            if (p <= m2 && v[p]) {
-                vc[i].push_back(p - i);
-            }
-            p++;
-        }
-    }
-
-    ll ma = 0;
-    for (auto i : ii) {
-        ma += vc[i].empty() ? 0 : vc[i].back();
-    }
-    ans[0] = true, ans[ma] = true;
-    vector<ll> tmp;
-    tmp.reserve(100000);
-    unordered_set<ll> jt;
-    for (int i = 1; i < ma; i++) {
-        jt.insert(i);
-    }
-    for (auto i : ii) {
-        tmp.clear();
-        for (auto j : jt)
-            for (auto offset : vc[i])
-                if (j - offset >= 0 && ans[j - offset])
-                    tmp.push_back(j);
-        for (auto& tt : tmp) {
-            jt.erase(tt);
-            ans[tt] = true;
-        }
-    }
-    for (int i = 0; i <= ma; i++) {
-        if (ans[i])
-            cout << i << " ";
+        q.insert(t);
+        if (i & 1)
+            cout << q.getans() << endl;
     }
 }
 
--- tt/3.26/12.cpp
+++ tt/3.26/12.cpp
@@ -6,6 +6,9 @@
 #include <string>
 #include <vector>
 
+#undef INT32_MAX
+#define INT32_MAX 99999
+
 using namespace std;
 using ll = long long;
 
@@ -24,11 +27,21 @@ int f[111];
 
 int n, d;
 
+void output(int i, int j) {
+    if (p[i][j] == j)
+        return;
+
+    auto& t = ns[p[i][j]];
+    cout << t.x << " " << t.y << endl;
+
+    output(p[i][j], j);
+}
+
 void solve() {
     cin >> n >> d;
-    g[0] = { 0, 0 };
+    ns.resize(n + 3);
     for (int i = 1; i <= n; i++) {
-        cin >> g[i].x >> g[i].y;
+        cin >> ns[i].x >> ns[i].y;
     }
 
     for (int i = 0; i <= n + 10; i++)
@@ -42,11 +55,11 @@ void solve() {
             int x2 = ns[j].x, y2 = ns[j].y;
             int dd = (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
             if (dd <= d * d)
-                gra[i][j] = 1, p[i][j] = j, p[j][i] = i;
+                gra[i][j] = 1, p[i][j] = j;
         }
     }
 
-    for (int i = 1;i <= n;i++) {
+    for (int i = 1; i <= n; i++) {
         int x1 = ns[i].x, y1 = ns[i].y;
         int di = min(50 - abs(x1), 50 - abs(y1));
         if (di <= d) {
@@ -55,30 +68,34 @@ void solve() {
         }
     }
 
-    for (int i = 1;i <= n;i++) {
+    for (int i = 1; i <= n; i++) {
         int x1 = ns[i].x, y1 = ns[i].y;
+        f[i] = INT32_MAX;
 
-        if (x1 * x1 + y1 * y1 <= (15 + d) * (15 + d)) {
+        if (x1 * x1 + y1 * y1 <= (7.5 + d) * (7.5 + d)) {
             gra[0][i] = 1;
             p[0][i] = i;
+            f[i] = x1 * x1 + y1 * y1;
         }
     }
 
-    for (int k = 1;k <= n + 1;k++)
-        for (int i = 1;i <= n + 1;i++)
-            for (int j = 1; j <= n + 1;j++) {
-                int t = gra[i][k] + gra[k][j];
+    for (int k = 1; k <= n; k++)
+        for (int i = 0; i <= n; i++)
+            for (int j = 1; j <= n + 1; j++) {
+                ll t = gra[i][k] + gra[k][j];
                 if (gra[i][j] > t ||
-                    (i == 0 && t == gra[i][j] && f[p[0][k]] < f[p[0][j]])
-                    ) {
+                    (i == 0 && t == gra[i][j] && f[p[0][k]] < f[p[0][j]])) {
                     gra[i][j] = gra[i][k] + gra[k][j];
-                    p[i][j] = k;
+                    p[i][j] = p[i][k];
                 }
             }
 
-
-
-
+    if (gra[0][n + 1] == INT32_MAX)
+        cout << 0 << endl;
+    else {
+        cout << gra[0][n + 1] << endl;
+        output(0, n + 1);
+    }
 }
 
 int main() {
