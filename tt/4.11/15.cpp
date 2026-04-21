50ddd0341ae637d3341d184f074202f1f9ca47f4
Sophia7320 <2060205344@qq.com>
2026-04-21 19:12:55 +0800

    .

--- tt/4.11/15.cpp
+++ tt/4.11/15.cpp
@@ -10,8 +10,66 @@ using ll = long long;
 using vll = vector<long long>;
 using pq = priority_queue<ll>;
 
-void _() {
+const ll M = 998244353;
 
+ll c[11100];
+
+vll dn[11100];
+
+ll sc[110];
+
+ll vis[45];
+
+ll dp[50][50];
+
+int n;
+
+ll pow(ll a, ll b) {
+    ll res = 1;
+    while (b) {
+        if (b & 1) res = res * a % M;
+        a *= a, b >>= 1;
+    }
+    return res;
+}
+
+void init_sc(int u) {
+    for (auto v : dn[u]) {
+        init_sc(v);
+        sc[u] += sc[v];
+        vis[u] |= vis[v];
+    }
+    vis[u] |= (1 << u);
+    ++sc[u];
+}
+
+
+void dfs(int cur) {
+    ///dp[cur][1] = 1;
+
+    for (auto v : dn[cur]) dfs(v);
+
+    dp[cur][1] = pow(c[1], sc[cur]);
+
+}
+
+void _() {
+    cin >> n;
+    for (int i = 2;i <= n;i++) {
+        ll t;cin >> t;
+        dn[t].push_back(i);
+    }
+
+    for (int i = 1;i <= n;i++) cin >> c[i];
+
+    init_sc(1);
+    dfs(1);
+
+    ll ans = 0;
+    for (int k = 1;k <= n;k++) {
+        ans = (ans + dp[1][k]) % M;
+    }
+    cout << ans << endl;
 }
 
 int main() {
--- tt/4.11/5.cpp
+++ tt/4.11/5.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <algorithm>
 #include <queue>
+#include <set>
+#include <iomanip>
 
 //#define endl "\n"
 
@@ -10,14 +12,29 @@ using ll = long long;
 using vll = vector<long long>;
 using pq = priority_queue<ll>;
 
-void _() {
+ll cnt(int n) {
+    int i = 0;set<int> s;
+    while (n) {
+        i++;
+        s.insert(n % 10), n /= 10;
+    }
+    if (i < 4)s.insert(0);
+    return s.size();
+}
 
+void _() {
+    int y, n;cin >> y >> n;
+    int t = y;
+    while (cnt(y) != n) y++;
+    cout << y - t << " " << setw(4) << y;
 }
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
+
+    cout << setfill('0');
 
     int t = 1;
     // cin >> t ;
